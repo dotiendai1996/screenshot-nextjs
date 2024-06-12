8609d12bf0dd3b1e9823e814d3b898c98c284863
Dai Do <dotiendai.dtd@gmail.com>
2024-06-12 15:59:37 +0700

    test browserless

--- app/lib/browser.ts
+++ app/lib/browser.ts
@@ -1,13 +1,24 @@
-const puppeteer = require('puppeteer');
+// const puppeteer = require('puppeteer');
+import edgeChromium from 'chrome-aws-lambda';
+import puppeteer from 'puppeteer-core';
+
+const LOCAL_CHROME_EXECUTABLE = '/Applications/Google Chrome.app/Contents/MacOS/Google Chrome';
 
 export default async function startBrowser() {
 	let browser;
+	// Edge executable will return an empty string locally.
+	const executablePath = await edgeChromium.executablePath || LOCAL_CHROME_EXECUTABLE
 	try {
 	    console.log("Opening the browser......");
-	    browser = await puppeteer.launch({
-	        headless: false,
-	        args: ["--disable-setuid-sandbox"],
-	        'ignoreHTTPSErrors': true
+	    browser = await edgeChromium.puppeteer.launch({
+	        // headless: false,
+	        // args: ["--disable-setuid-sandbox"],
+			// 'ignoreHTTPSErrors': true
+			args: edgeChromium.args,
+			defaultViewport: edgeChromium.defaultViewport,
+			executablePath: await edgeChromium.executablePath,
+			headless: edgeChromium.headless,
+			ignoreHTTPSErrors: true,
 	    });
 	} catch (err) {
 	    console.log("Could not create a browser instance => : ", err);
--- app/lib/pageScraper.ts
+++ app/lib/pageScraper.ts
@@ -1,15 +1,15 @@
 const scraperObject = {
 	async scraper(browser: any, tracking_no: string){
-		let url = `https://tools.usps.com/go/TrackConfirmAction?qtc_tLabels1=${tracking_no}`;
+		let url = `https://ops.hanoinhanh.com/auth/sign-in`;
 		let page = await browser.newPage();
 		console.log(`Navigating to ${url}...`);
 		await page.goto(url);
 		// Wait for the required DOM to be rendered
 		async function scrapeCurrentPage() {
 			try {
-				await page.waitForSelector('p.tb-status-detail', {timeout: 10000}); // Need to change
+				await page.waitForSelector('p.MuiTypography-root', {timeout: 10000}); // Need to change
 				// console.log('Done wait selector');
-				let values = await page.$$eval('p.tb-status-detail', (els: any) => {
+				let values = await page.$$eval('p.MuiTypography-root', (els: any) => {
 					els = els.map((el: any) => el.textContent);
 					return els;
 				});
