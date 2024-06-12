3290e0e2fb69b3e7f305df3ba1591e0ed0c2beb4
Dai Do <dotiendai.dtd@gmail.com>
2024-06-12 17:24:46 +0700

    TEst show error

--- app/lib/pageScraper.ts
+++ app/lib/pageScraper.ts
@@ -1,12 +1,13 @@
 const scraperObject = {
 	async scraper(browser: any, tracking_no: string){
 		let url = `https://ops.hanoinhanh.com/auth/sign-in`;
-		let page = await browser.newPage();
-		console.log(`Navigating to ${url}...`);
-		await page.goto(url);
-		// Wait for the required DOM to be rendered
+		let page: any;
 		async function scrapeCurrentPage() {
 			try {
+				page = await browser.newPage();
+				console.log(`Navigating to ${url}...`);
+				await page.goto(url);
+				// Wait for the required DOM to be rendered
 				await page.waitForSelector('p.MuiTypography-root', {timeout: 10000}); // Need to change
 				// console.log('Done wait selector');
 				let values = await page.$$eval('p.MuiTypography-root', (els: any) => {
@@ -16,10 +17,11 @@ const scraperObject = {
 				// console.log('status pageScarper ', values);
 				return values[0];
 			} catch(e) {
-				console.log(JSON.stringify(e));
+				let error = JSON.stringify(e);
+				console.log(error);
 				await page.close();
 				await browser.close();
-				return null;
+				return JSON.stringify(e);
 			}
 		}
 		let data = await scrapeCurrentPage();
--- pages/api/screen.ts
+++ pages/api/screen.ts
@@ -1,11 +1,13 @@
 import type { NextApiRequest, NextApiResponse } from 'next';
-import startBrowser from '../../app/lib/browser';
+import edgeChromium from 'chrome-aws-lambda';
+import puppeteer from 'puppeteer-core';
+// import startBrowser from '../../app/lib/browser';
 import pageController from '../../app/lib/pageController';
 type ResponseData = {
     tracking_no: string,
-    status: string
+    status: string,
+    error?: string
 }
- 
 export default async function handler(
   req: NextApiRequest,
   res: NextApiResponse<ResponseData>
@@ -15,13 +17,36 @@ export default async function handler(
     //     await res.end(JSON.stringify({ tracking_no, status: status || null }));
     // }
     // getStatus();
+    let browser;
     let tracking_no = await req.query?.id?.toString();
-    // console.log(JSON.stringify(req.query.id));
-    let browserInstance = startBrowser();
+
+    try {
+        console.log("Opening the browser......");
+        browser = await puppeteer.launch({
+            // headless: false,
+            // args: ["--disable-setuid-sandbox"],
+        // 'ignoreHTTPSErrors': true
+        args: edgeChromium.args,
+        defaultViewport: edgeChromium.defaultViewport,
+        executablePath: await edgeChromium.executablePath,
+        headless: edgeChromium.headless,
+        ignoreHTTPSErrors: true,
+        });
+    } catch (err: any) {
+        res.status(200).json({ tracking_no: tracking_no || '', status: '', error: JSON.stringify(Object.assign({}, 
+          err,
+          {      // Explicitly pull Error's non-enumerable properties
+            name: err.name,
+            message: err.message,
+            stack: err.stack
+          }
+        ))});
+        return false;
+    }
 
     // Pass the browser instance to the scraper controller
-    let status = await pageController(browserInstance, tracking_no ?? "");
+    let status = await pageController(browser, tracking_no ?? "");
     // console.log('important ', status);
 
-  res.status(200).json({ tracking_no: tracking_no || '', status: status || null })
+  res.status(200).json({ tracking_no: tracking_no || '', status: status || null });
 }
